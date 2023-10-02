6945f7fa62195eb285fc058b6c082732f595c82b
Phuongthao2k <18521422@gm.uit.edu.vn>
2023-10-02 20:44:40 +0700

    2/10

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,15 +4,12 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required] public string Username { get; set; }
-        public object UserName { get; internal set; }
-        [Required] public string KnownAs { get; set; }
-        [Required] public string Gender { get; set; }
-        [Required] public DateOnly? DateOfBirth { get; set; }
-        [Required] public string City { get; set; }
-        [Required] public string Country { get; set; }
-
-        [Required]
+        public string Username { get; set; }
+        public string KnownAs { get; set; }
+        public string Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
