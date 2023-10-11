788e83caefb0c63eb1f11338e58b1cdaa59556e8
Dương Thanh Nam <62831022+thanhnamrei@users.noreply.github.com>
2023-10-11 22:42:24 +0700

    feat: pagination

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
@@ -11,7 +12,7 @@ namespace API.Interfaces
 
 		Task<AppUser> GetUserByUsernameAsync(string username);
 
-		Task<IEnumerable<MemberDto>> GetMembersAsync();
+		Task<IEnumerable<MemberDto>> GetMembersAsync(UserParams userParams);
 
 		Task<MemberDto> GetMemberAsync(string username);
 
